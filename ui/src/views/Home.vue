83a423407c3b8ebef8108b2b807960f2c1719732
Max Heckel <maxheckel@Maxs-MacBook-Pro.local>
2019-04-03 23:12:26 -0400

    Started on UI

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js Testing"/>
+    <BigButton title="Listen"/>
+    <BigButton title="+ Album"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import BigButton from "../components/BigButton";
 
 export default {
   name: 'home',
   components: {
+    BigButton,
     HelloWorld
   }
 }
